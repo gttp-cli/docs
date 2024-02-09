57729bf1266376ace1c6128aeb17b93b59217de7
MarvinJWendt <git@marvinjwendt.com>
2024-02-09 18:25:52 +0100

    docs: updated categories

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,17 +61,10 @@ const config = {
       require.resolve("@easyops-cn/docusaurus-search-local"),
       /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
       ({
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
         hashed: true,
         indexBlog: false,
         docsRouteBasePath: '/',
         language: "en",
-
-        // For Docs using Chinese, The `language` is recommended to set to:
-        // ```
-        // language: ["en", "zh"],
-        // ```
       }),
     ],
   ],
@@ -156,7 +149,7 @@ const config = {
       },
       prism: {
         theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
+        darkTheme: prismThemes.oceanicNext,
       },
     }),
 };
